dc8baa7e45bd2e1d0a48f2b882420eb640b5642e
jacob herndon <jacobronaldhernon@gmail.com>
2023-06-04 17:38:22 -0600

    chnges to the bankacount class

--- Class Projects/BankAccount.py
+++ Class Projects/BankAccount.py
@@ -16,16 +16,16 @@
 
 class BankAccount:
 
-    Owner: str
-    CoOwners: list[str]
+    Owner: str = ""
+    CoOwners: list[str] = []
 
-    Balance: float
-    Deposits: list[float]
-    Withdrawals: list[float]
+    Balance: float = 0.0
+    Deposits: list[float] = []
+    Withdrawals: list[float] = []
 
-    IntrestRate: float
+    IntrestRate: float = 0.0
 
-    def __init__(self, owner: str, coOwners: list[str] = [""], firstDeposit: float = 0.0, intrestRate: float = 3.0):
+    def __init__(self, owner: str, coOwners: list[str] = [], firstDeposit: float = 0.0, intrestRate: float = 3.0):
         self.Owner = owner
         self.CoOwners = coOwners
         self.Balance = firstDeposit
@@ -33,54 +33,16 @@ class BankAccount:
             self.Deposits.append(firstDeposit)
         self.IntrestRate = intrestRate
 
-    def Deposit(self,Diposit1):
-        self.Balance += Diposit1
-        if Diposit1 != 0.0:
-            self.Deposits.append(Diposit1)
-        
-        
-        
+    def Deposit(self, thisDeposit: float):
+        self.Balance += thisDeposit
+        self.Deposits.append(thisDeposit)
 
-    def Withdrawal(self,Withdrawal1):
-        self.Balance -= Withdrawal1
-        if Withdrawal1 != 0.0:
-            self.Withdrawals.append(Withdrawal1)
+    def Withdrawal(self, thisWithdrawal: float):
+        self.Balance -= thisWithdrawal
+        self.Withdrawals.append(thisWithdrawal)
 
-    def __add__(self,addCoOwner):
-        
-        pass
+    def AddCoOwner(self, coOwner: str):     
+        self.CoOwners.append(coOwner)
 
-    def removeCoOwner(self,remCoOwner):
-        
-        pass
-        
-        
-        
-
-        
-        
-
-    # Function: Deposit
-    #   - Change the account balance accordingly
-    #   - Record the deposit in 'Deposits'
-    #
-    # Function: Withdrawal
-    #   - Change the account balance accordingly
-    #   - Record the withdrawal in 'Withdrawals'
-    #
-    # Function: Add CoOwner
-    #   - Add the co-owner to 'CoOwners'
-    #
-    # Function: Remove CoOwner
-    #   - Remove the co-owner from 'CoOwners'
-
-myAccount = BankAccount(owner="Jacob",)
-
-myAccount.Deposit(2500.0)
-myAccount.Withdrawal(1500.0)
-myAccount.Deposit(20.0)
-myAccount.Withdrawal(15.0)
-myAccount.AddCoOwner(["Taylor"])
-myAccount.remove
-
-print(f"Deposits:\n{myAccount.Deposits}\n\nWithdrawals:\n{myAccount.Withdrawals}\n\nBalance:\n{myAccount.Balance}")
+    def RemoveCoOwner(self, coOwner: str):
+        self.CoOwners.remove(coOwner)
