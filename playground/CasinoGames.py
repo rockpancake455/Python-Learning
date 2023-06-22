e5f9bc1e9cd9c0ab10188000e282456ace15039e
jacob herndon <jacobronaldherndon@gmail.com>
2023-06-21 20:12:57 -0600

    Co-authored-by: TaylorBryanHerndon@gmail.com <TaylorBryanHerndon@gmail.com> addons to the playground that changed to cosino and started to make a --

---
+++ playground/CasinoGames.py
@@ -0,0 +1,14 @@
+#
+# Make a list of symbols (BAR, cherry, lollypop, etc...)
+# Make the game cost 10 coins to play
+# If the user doesn't have 10 coins they can't play!
+# Pick a symbol at random for all three slots
+# if all three slots match up you win the jackpot of 200 coins!
+# if two symbols match you win the mini pot of 20 coins!
+# if get a BAR then you automatically lose.
+#
+def Slots():
+    pass
+
+def Chicken():
+    print("Playing Chicken")
